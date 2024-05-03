bee7c52d6e702935b4d8b83a26e9664d902f971d
basic <76001612+hassan-basic@users.noreply.github.com>
2024-05-03 15:21:26 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from kivy.app import App
-from kivy.lang.builder import Builder
-class Myapp(App):
-    def build(self):return Builder.load_string("""Button:
-        text:'click'
-        on_release:self.text='exit' if self.text=='click' else setattr(self,'on_release',exit())
-        """)
-if __name__ == '__main__':Myapp().run()
